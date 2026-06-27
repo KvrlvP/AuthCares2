0892178436c97d526347c5b2682dc516f655f2ba
KvrlvP <paolaz2005d@gmail.com>
2026-06-27 12:46:15 -0500

    style: añade paleta para navegación flotante

--- app/src/main/java/com/choque/authcares2/ui/theme/Color.kt
+++ app/src/main/java/com/choque/authcares2/ui/theme/Color.kt
@@ -29,6 +29,11 @@ val AuthCaresSurfaceContainerLowest = Color(0xFFFFFFFF)
 val AuthCaresWhiteSurface = Color(0xFFFFFFFF)
 val AuthCaresSurfaceDim = Color(0xFFD9DAE1)
 
+val AuthCaresNavigationGlass = Color(0xE6EAF0F8)
+val AuthCaresNavigationSelected = Color(0xFFD7E5FF)
+val AuthCaresNavigationBorder = Color(0xB3FFFFFF)
+val AuthCaresNavigationShadow = Color(0x33001840)
+
 val AuthCaresOnSurface = Color(0xFF1A1C1E)
 val AuthCaresOnSurfaceVariant = Color(0xFF43474E)
 val AuthCaresOutlineVariant = Color(0xFFC4C6CF)
